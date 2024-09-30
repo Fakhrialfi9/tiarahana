bbfa807016d38e6b8fad0d5c7cc233e7aaa0b284
Fakhrialfi9 <Fakhrialfi9@gmail.com>
2024-09-30 14:45:02 +0700

    Update main.jsx
    edit

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./base/apps/app.tsx";
 import "./base/style/index.css";
 import "./base/style/components.css";
-import "./base/style/lightColor.css";
-// import "./base/style/darkColor.css";
 import "./base/style/form.css";
 import "./base/style/framework.css";
 import "./base/style/responsive.css";
